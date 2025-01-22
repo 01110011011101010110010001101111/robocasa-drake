190a4f8597c62da99a6469694d3e38daad8d9a5f
sudo <41024739+01110011011101010110010001101111@users.noreply.github.com>
2025-01-22 18:42:00 -0500

    script fix

--- run_robocasa.sh
+++ run_robocasa.sh
@@ -5,6 +5,6 @@ for i in {0..9}; do
     # Loop through values 0-11 for the second value
     for j in {0..11}; do
         # Run the command with the current combination of i and j
-        echo "$i $j" | python -m robocasa.demos.demo_kitchen_scenes
+        echo -e "$i\n$j" | python -m robocasa.demos.demo_kitchen_scenes
     done
 done
